99edaef7429fb8e3a5693c9ab3bef74ceffa2b3e
Mist <mist.zzh@gmail.com>
2024-09-23 16:11:41 +0800

    ci: add prettier in code quality checker

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 // @ts-check
 
 import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import tslint from 'typescript-eslint'
+import eslintConfigPrettier from "eslint-config-prettier";
 
-export default tseslint.config({
+export default tslint.config({
   files: ['**/*.ts'],
-  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  extends: [eslint.configs.recommended, ...tslint.configs.recommended, eslintConfigPrettier],
   rules: {
     'no-console': 'warn',
     '@typescript-eslint/no-unused-vars': 'error'
