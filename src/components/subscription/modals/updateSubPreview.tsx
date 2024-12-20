9cf7722fa1ee0d1b85dc13026243d1ff774af76e
Edward Wo <edward.wo@wowow.io>
2024-12-20 11:30:23 +0800

    Feat/use credit in upgrade plan (#123)
    * chore: remove unused var
    
    * chore: cleanup
    
    * fix: missing message import
    
    * feat: add credit use in upgrade sub preview modal
    
    * chore: remove used var
    
    * fix: pass credit info to create_submit
    
    * chore: cleanup

--- src/components/subscription/modals/updateSubPreview.tsx
+++ src/components/subscription/modals/updateSubPreview.tsx
@@ -15,13 +15,6 @@ interface Props {
   onAfterConfirm: () => void
 }
 
-/*
-     subscriptionId,
-      newPlanId,
-      quantity: 1,
-      addonParams: addons,
-      discountCode
-*/
 const updateSubPreview = ({
   subscriptionId,
   newPlanId,
@@ -68,17 +61,24 @@ const updateSubPreview = ({
     if (subscriptionId === undefined || previewInfo === null) {
       return
     }
-
-    setConfirming(true)
-    const [updateSubRes, err] = await updateSubscription(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const body: any = {
       subscriptionId,
       newPlanId,
       addons,
-      previewInfo.totalAmount,
-      previewInfo.currency,
-      previewInfo.prorationDate,
-      discountCode
-    )
+      confirmTotalAmount: previewInfo.totalAmount,
+      confirmCurrency: previewInfo.currency,
+      prorationDate: previewInfo.prorationDate,
+      discountCode: discountCode,
+      applyPromoCredit: creditAmt != null && creditAmt >= 0,
+      applyPromoCreditAmount: creditAmt
+    }
+    if (!body.applyPromoCredit) {
+      delete body.applyPromoCredit
+      delete body.applyPromoCreditAmount
+    }
+    setConfirming(true)
+    const [updateSubRes, err] = await updateSubscription(body)
     setConfirming(false)
     if (null != err) {
       message.error(err.message)
@@ -141,7 +141,9 @@ const updateSubPreview = ({
           {previewInfo !== null && (
             <SubtotalInfo
               iv={previewInfo.nextPeriodInvoice}
-              isCreditUsed={previewInfo.applyPromoCredit}
+              isCreditUsed={
+                previewInfo.nextPeriodInvoice.promoCreditDiscountAmount != 0
+              }
             />
           )}
 
@@ -155,7 +157,7 @@ const updateSubPreview = ({
           {previewInfo !== null && (
             <SubtotalInfo
               iv={previewInfo.invoice}
-              isCreditUsed={previewInfo.applyPromoCredit}
+              isCreditUsed={previewInfo.invoice.promoCreditDiscountAmount != 0}
             />
           )}
         </>
