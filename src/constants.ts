680dabcb1689bf476c8408527378f7426a045555
Edward <edward.wo@wowow.io>
2025-01-21 09:52:02 +0800

    fix: RUB not in currency constant

--- src/constants.ts
+++ src/constants.ts
@@ -70,6 +70,7 @@ export const CURRENCY: {
   CNY: { symbol: '¥', stripe_factor: 100, decimal_places: 2 },
   USD: { symbol: '$', stripe_factor: 100, decimal_places: 2 },
   JPY: { symbol: '¥', stripe_factor: 1, decimal_places: 0 },
+  RUB: { symbol: '₽', stripe_factor: 100, decimal_places: 2 },
   EUR: { symbol: '€', stripe_factor: 100, decimal_places: 2 },
   USDT: { symbol: '₮', stripe_factor: 100, decimal_places: null }
 }
