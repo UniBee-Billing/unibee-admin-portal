2805fe7d741972d4eada709bea6b2b6fe89a333b
Edward Wo <edward.wo@wowow.io>
2025-02-13 01:51:26 +0800

    style: redesign <logout />, <about /> in sidebar (#213)

--- src/components/sidebar/logo.tsx
+++ src/components/sidebar/logo.tsx
@@ -1,13 +1,13 @@
+import { Image } from 'antd'
 import { useMerchantInfoStore } from '../../stores'
 import { withEnvBasePath } from '../../utils'
-
-export const Logo = () => {
+export const Logo = ({ collapsed }: { collapsed: boolean }) => {
   const merchantInfoStore = useMerchantInfoStore()
 
   return (
     <div className="my-4 flex justify-center text-white">
-      <img
-        className="h-20"
+      <Image
+        className={`${collapsed ? 'w-10' : 'w-20'} h-20 transition-all duration-300`}
         src={
           merchantInfoStore.companyLogo ||
           withEnvBasePath('/logoPlaceholder.png')
