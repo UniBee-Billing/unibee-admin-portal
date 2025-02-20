3ecd1202a5ff51a8c5a60e827c4f42449aff98fd
Edward Wo <edward.wo@wowow.io>
2025-02-20 22:25:12 +0800

    feat: add user info popover on bottom-left icon (#248)

--- src/components/sidebar/logoWithAction.tsx
+++ src/components/sidebar/logoWithAction.tsx
@@ -1,10 +1,14 @@
+import { Popover } from 'antd'
+import { ReactNode } from 'react'
+
 const LogoWithAction = ({
   collapsed,
   clickHandler,
   text,
   logoColor,
   logo,
-  height
+  height,
+  popoverText
 }: {
   collapsed: boolean
   clickHandler?: () => void
@@ -12,27 +16,48 @@ const LogoWithAction = ({
   logo?: React.ReactNode
   logoColor?: string
   height?: string
+  popoverText?: ReactNode
 }) => (
-  <div
-    style={{ height: height ?? '40px' }}
-    className={`relative flex w-full ${clickHandler != undefined && 'cursor-pointer'} items-center pl-7 opacity-80 hover:text-gray-900 hover:opacity-100`}
-    onClick={clickHandler}
-  >
-    {logo != undefined && (
-      <div
-        className={`h-4 w-4 flex-shrink-0 ${logoColor} transition-all duration-300`}
-      >
-        {logo}
-      </div>
-    )}
+  <PopoverWrapper needPopover={popoverText != undefined} content={popoverText}>
     <div
-      className={`${logo != undefined && 'ml-3'} flex-shrink-0 text-gray-100 ${
-        collapsed ? 'opacity-0' : 'opacity-100'
-      } transition-opacity duration-300`}
+      style={{ height: height ?? '40px' }}
+      className={`relative flex w-full ${clickHandler != undefined && 'cursor-pointer'} items-center pl-7 opacity-80 hover:text-gray-900 hover:opacity-100`}
+      onClick={clickHandler}
     >
-      {text}
+      {logo != undefined && (
+        <div
+          className={`h-4 w-4 flex-shrink-0 ${logoColor} transition-all duration-300`}
+        >
+          {logo}
+        </div>
+      )}
+      <div
+        className={`${logo != undefined && 'ml-3'} flex-shrink-0 text-gray-100 ${
+          collapsed ? 'opacity-0' : 'opacity-100'
+        } transition-opacity duration-300`}
+      >
+        {text}
+      </div>
     </div>
-  </div>
+  </PopoverWrapper>
 )
 
 export default LogoWithAction
+
+const PopoverWrapper = ({
+  children,
+  needPopover,
+  content
+}: React.PropsWithChildren<{ content?: ReactNode; needPopover: boolean }>) => {
+  return !needPopover ? (
+    <>{children}</>
+  ) : (
+    <Popover
+      content={content}
+      placement="top"
+      overlayStyle={{ maxWidth: '360px' }}
+    >
+      {children}
+    </Popover>
+  )
+}
